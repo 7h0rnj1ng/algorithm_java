8b2683f97fdf4c1956977e6ecf8323b4406e9c6f
7h0rnj1ng <7h0rnj1ng@gmail.com>
2020-03-03 00:13:39 +0800

    剑指offer 005 替换字符串

---
+++ Sword_finger_offer/src/main/string_question/Solution005.java
@@ -0,0 +1,40 @@
+import java.security.ProtectionDomain;
+
+public class Solution005 {
+    public String replaceSpace(StringBuffer str) {
+        /* 统计出空格的个数*/
+        int spacenum = 0;
+        for (int i = 0; i <str.length() ; i++) {
+            if (str.charAt(i) ==' '){
+                spacenum++;
+            }
+        }
+        /*讲str设置为新长度的字符串，设置两个指针，从后向前复制*/
+        int oldstrlength = str.length();
+        int newstrlenlength = oldstrlength+spacenum*2;
+        str.setLength(newstrlenlength);
+        int oldindex = oldstrlength-1;
+        int newindex = newstrlenlength-1;
+        for (;oldindex>=0&&oldindex<newstrlenlength;oldindex--) {
+            if (str.charAt(oldindex) == ' ') {
+                str.setCharAt(newindex--, '0');
+                str.setCharAt(newindex--, '2');
+                str.setCharAt(newindex--, '%');
+            } else {
+                str.setCharAt(newindex--, str.charAt(oldindex));
+            }
+
+        }
+        return str.toString();
+
+    }
+
+    public static void main(String[] args) {
+
+        StringBuffer str = new StringBuffer();
+        str.append("we are family ");
+        Solution005 solution = new Solution005();
+        System.out.print(solution.replaceSpace(str));
+
+    }
+}
